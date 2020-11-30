c7055387d2cdaf9cbd5845d2e038a7318de5569a
Crowdbotics <team@crowdbotics.com>
2020-11-30 08:09:25 +0000

    Update components

--- src/features/BlankScreen115135/screens/blank.js
+++ src/features/BlankScreen115135/screens/blank.js
@@ -27,17 +27,23 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View>
-      <Button title="Press me!" onPress={() => alert("Pressed!")} />
-      <Text>Sample text content</Text>
+      <Button
+        title="Press me!"
+        style={styles.Button_2}
+        onPress={() => alert("Pressed!")}
+      />
+      <Text style={styles.Text_3}>Sample text content</Text>
       <Image
         source={{
           uri:
             "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_unCbTwQ.jpg"
         }}
+        style={styles.Image_4}
       />
       <TextInput
         placeholder="Sample text input placeholder"
         multiline={true}
+        style={styles.TextInput_5}
         value={this.state.TextInput_5}
         onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
       />
@@ -50,9 +56,41 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
   Button_2: {},
   Text_3: {},
   Image_4: {},
-  TextInput_5: {}
+  TextInput_5: {},
+  View_1: {},
+  Button_2: {
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  },
+  Text_3: {
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  },
+  Image_4: {
+    borderColor: "#9d58ca",
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  },
+  TextInput_5: {
+    height: 100,
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  }
 })
